1b5b1b5c06d99d51e5769e48cfbee316f905fdcd
willriggins <williamriggins2013@gmail.com>
2016-06-07 11:18:00 -0400

    add login

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -15,10 +16,42 @@ public class Main {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
                     HashMap m = new HashMap();
-                    return new ModelAndView(m, "login.html");
+                    if (username == null) {
+                        return new ModelAndView(m, "login.html");
+                    }
+                    else {
+                        return new ModelAndView(m, "home.html");
+                    }
                 },
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String name = request.queryParams("username");
+                    String pass = request.queryParams("password");
+                    if (name == null || pass == null) {
+                        throw new Exception("Name or pass not sent");
+                    }
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name, pass);
+                        users.put(name, user);
+                    }
+                    else if (!name.equals(user.name)) {
+                        throw new Exception("Wrong password");
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", name);
+
+                    response.redirect("/");
+                    return "";
+                }
+
         );
     }
 }
